70a6206ca6c66826a4dd2adfe75b5521edb1642d
Matthew Parias <60233752+maparias@users.noreply.github.com>
2023-05-01 14:04:51 -0400

    Update TFTPfinal.py

--- TFTPfinal.py
+++ TFTPfinal.py
@@ -1,21 +1,20 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sun Apr 30 20:46:30 2023
 
-@author: matth
-"""
 
 import socket
 
+#opcode denotaton
 RRQ_OPCODE = 1
 WRQ_OPCODE = 2
 DATA_OPCODE = 3
 ACK_OPCODE = 4
 ERROR_OPCODE = 5
 
+#able to chnage, made these as basic values
 MAX_PACKET_SIZE = 516
 MAX_DATA_SIZE = 512
 
+#method to create a packet and fill
 def create_packet(opcode, block_number=0, data=b''):
     packet = bytearray()
     packet += b'\x00'
@@ -24,12 +23,14 @@ def create_packet(opcode, block_number=0, data=b''):
     packet += data
     return bytes(packet)
 
+#parses though the packet and returns bytearray of data along with opcode and block size for processing
 def parse_packet(packet):
     opcode = packet[1]
     block_number = int.from_bytes(packet[2:4], byteorder='big')
     data = packet[4:]
     return opcode, block_number, data
 
+#sendng packets over  socket
 def send_tftp_packet(sock, opcode, block_number=0, data=b'', addr=None):
     packet = create_packet(opcode, block_number, data)
     if addr is None:
@@ -38,6 +39,7 @@ def send_tftp_packet(sock, opcode, block_number=0, data=b'', addr=None):
         sock.sendto(packet, addr)
     return packet
 
+#recieving packtes. Calls for an RRQ from either client and requires a reponse of packet and such left
 def tftp_download_file(server_addr, filename):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     data = bytearray()
@@ -52,6 +54,8 @@ def tftp_download_file(server_addr, filename):
                     return bytes(data)
             elif opcode == ERROR_OPCODE:
                 raise RuntimeError(f'TFTP error: {packet_data.decode()}')
+                #require response and cancels without one
+                
     except EOFError:
         print("no data provided to input function")
         sock.close()
