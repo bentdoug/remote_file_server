43b6498c8dd811b2b96e0d520b21000d4687c5fd
Benjamin Douglas <74840985+bentdoug@users.noreply.github.com>
2023-05-06 14:29:39 -0400

    Adding Documentation

--- client/controller.py
+++ client/controller.py
@@ -26,6 +26,10 @@ class client:
         UI.call_fn(self)
 
     def listen_for_server_cmd(self):
+        '''
+        Listens for an OPCODE from the server connection and initiates the appropriate 
+        functions upon receiving one
+        '''
         state = None
         while state is not OPCODES["Done"]:
             recvd = self.comms.receive_opcode()
@@ -38,13 +42,24 @@ class client:
                 state = OPCODES["Done"]
 
     def push_file(self, file):
+        '''
+        Facilitates adding a file from the client to the server
+
+        Parameters:
+            file (str):
+                A string representing the file to be sent's name
+        '''
         self.comms.send_opcode(OPCODES["add_file"], file.split('/')[-1])
         self.send_file_contents(file)
 
-    def pull_file(self, file):
-        pass
-
     def send_file_contents(self, file):
+        '''
+        Function to send the contents of a file from the client to the server
+
+        Parameters:
+            file (str)
+                A string representing the file to be sent's name
+        '''
         with open(file, 'r') as f:
             file_contents = f.read()
         self.comms.send_file(file)
@@ -57,11 +72,21 @@ class client:
         self.comms.send_str(request_cmd)
 
     def send_file_hash(self, file):
+        '''
+        Sends the hash value of a specified file
+
+        Parameters:
+            file (str)
+                A string representing the file who's hash is to be sent's name
+        '''
         with open('bin/client/'+file, 'r') as f:
             file_contents = f.read()
         self.comms.send_hash(file, hash(file_contents))
 
     def disconnect(self):
+        '''
+        Facilitates the disconnection from the socket and subsequent ending of the program
+        '''
         self.comms.send_opcode(OPCODES["disconnect"])
         #self.listen_for_server_cmd()
         sys.exit()
--- comms/communications.py
+++ comms/communications.py
@@ -12,6 +12,10 @@ versions = ['Client', 'Server']
 
 class comms():
     def __init__(self, version):
+        '''
+        Initializes the comms object which facilitates all communication related
+        functions between client and server
+        '''
         self.version = version
         if version == 0:
             self.socket = connect.establish_server_connection(HOST, PORT)
@@ -19,20 +23,52 @@ class comms():
             self.socket = connect.receive_client_connection(HOST, PORT)[0]
 
     def send_str(self, str):
+        '''
+        Facilitates the sending of a string message through the socket
+
+        Parameters:
+            str (str):
+                The string to be sent through the socket
+        '''
         print("{} is sending {} as string".format(versions[self.version], str))
         self.socket.send(f"{str}".encode())
 
     def send_hash(self, file_name, hash):
+        '''
+        Facilitates the sending of a file's hash value through the socket connection
+
+        Parameters:
+            file_name (str):
+                A string representing the name of the file who's hash is to be sent
+
+            hash (int):
+                the hash value of the file who's hash is to be sent
+        '''
         to_send = f"\"{file_name}\" : \"{hash}\""
         to_send = "{" + to_send + "}"
         self.send_str(to_send)
 
     def send_opcode(self, opcode, file_name="None"):
+        '''
+        Facilitates sending an opcode through the socket connection
+
+        Parameters:
+            opcode (str):
+                The opcode that should be sent through the socket connection
+            file_name (str):
+                The file (if applicable) the OPCODE is in reference too
+        '''
         to_send = f"{opcode}{SEPARATOR}{file_name}"
         print("{} is sending {}".format(versions[self.version], to_send))
         self.socket.send(to_send.encode())
 
     def receive_opcode(self):
+        '''
+        Facilitates receiving an OPCODE through the socket connection
+
+        Returns:
+            The recieved opcode and file name that the opcode is in reference too
+        '''
         print('{} is listening for OPCODE'.format(versions[self.version]))
         received = self.socket.recv(BUFFER_SIZE).decode()
         print("got", received)
@@ -40,6 +76,13 @@ class comms():
         return [opcode, file_name]
 
     def send_file(self, file_path):
+        '''
+        Facilitates the sending of a specified file through the socket connection
+
+        Parameters:
+            file_path (str):
+                A string representing the file path to the file to be sent
+        '''
         print("{} is sending {}".format(versions[self.version], file_path))
         # get file size
         filesize = os.path.getsize(file_path)
@@ -59,6 +102,12 @@ class comms():
                 self.socket.sendall(bytes_read)
 
     def recieve_file(self):
+        '''
+        Facilitates receiving a file through the socket connection
+
+        Returns:
+            the file's name and the contents of the file in string format
+        '''
         print('{} is attempting to recieve a file'.format(versions[self.version]))
         received = self.socket.recv(BUFFER_SIZE).decode()
         filename, filesize = received.split(SEPARATOR)
@@ -81,8 +130,17 @@ class comms():
         return filename, str_of_file
 
     def recieve_str(self):
+        '''
+        Facilitates receiving a string through the socket connection
+
+        Returns:
+            The string recieved through the socket connection
+        '''
         received = self.socket.recv(BUFFER_SIZE).decode()
         return received
     
     def disconnect(self):
+        '''
+        Closes the socket connection
+        '''
         self.socket.close()
